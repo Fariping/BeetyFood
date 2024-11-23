55450aa7310f0d50b1705898c31b7bae0ba0cdc4
Fares <144069751+Fariping@users.noreply.github.com>
2024-11-23 12:05:24 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'amber': {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          800: '#92400e',
+          900: '#78350f',
+        },
+      },
+    },
   },
   plugins: [],
 }
